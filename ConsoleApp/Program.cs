f670d187cb4084b30442f3fb6c639cfc21bba25f
Behnam Emamian <emamian@persianprocess.com>
2018-05-21 23:06:12 +1000

    add .net framework console app

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿using MTProtoProxy;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
@@ -7,9 +11,21 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var mtprotoProxy = new MTProtoProxyServer("secret", 666);
+            var host = "127.0.0.1";
+            var port = 666;
+            //var secret = GenerateRandomSecret();
+            var secret = "656f293cb1234d3b89be35225b9a36a4";
+
+            Console.WriteLine("https://t.me/proxy?server=" + host + "&port=" + port + "&secret=" + secret);
+
+            var mtprotoProxy = new MTProtoProxyServer(secret, port, host);
             mtprotoProxy.Start();
             Console.ReadLine();
+        }
+
+        public static string GenerateRandomSecret()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
         }
     }
 }
