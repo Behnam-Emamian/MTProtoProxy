d0901ed6af4120b2f4524eb49b406dbfe7ad392a
Behnam Emamian <emamian@persianprocess.com>
2018-05-21 21:40:03 +1000

    add console application

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using MTProtoProxy;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mtprotoProxy = new MTProtoProxyServer("secret", 666);
+            mtprotoProxy.Start();
+            Console.ReadLine();
+        }
+    }
+}
